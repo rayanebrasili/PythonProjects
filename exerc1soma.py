e54fecead9db2610daaa4443263ccf592efc7278
Rayane Brasili <107143826+rayanebrasili@users.noreply.github.com>
2023-03-02 22:16:17 -0300

    Add files via upload

---
+++ exerc1soma.py
@@ -0,0 +1,7 @@
+print("\tSoma de dois números\n")
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+#Imprime resultado na tela
+input(num1+num2)
+print ("O resultado da soma é %d" %sum(num1, num2))
---
+++ exerc2media.py
@@ -0,0 +1,6 @@
+n1=int(input('Digite o primeiro numero:'))
+n2=int(input('Digite o segundo numero:'))
+n3=int(input('Digite o terceiro numero:'))
+
+
+print('A media é igual a',(n1+n2+n3/3))
